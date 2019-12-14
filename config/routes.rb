b6a1185c6c1c93a50538672fa815e81d131b8607
figueroar <raul.a.figueroa@hotmail.com>
2019-12-13 19:06:45 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources posts
+  get 'posts/index'
+  get 'posts/:id'
+  get 'posts/new'
+  get 'posts/:id/edit'
+
+  #resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :apartments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
